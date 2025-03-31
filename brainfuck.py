7a9123c603cff5738e8fcf4e8ee0894b314b92e7
BISC.lua <golf.le.fleur26@outlook.com>
2025-03-30 20:36:28 -0500

    Update brainfuck.py

--- brainfuck.py
+++ brainfuck.py
@@ -1,11 +1,24 @@
 # Name:Brainfuck Interpreter Python
-# Date: 2022/09/17
+# Original Date: 2022/09/17
+# Fork Date: 2025/03/30
 # Repository: https://github.com/BaseMax/BrainfuckInterpreterPython
 # Author: Max Base
+# Fork Author: Labcookie
+
+# CHANGELOG
+# - "+" sets the cell to 0 if it's at 255
+# - "-" sets the cell to 255 if it's at 0
+# - ">" sets the cell pointer to 0 if it's at 30,000
+# - "<" sets the cell pointer to 30,000 if it's at 0
+# - Implemented multi-line support
+# - New two arguments: stdin and size, if size empty, it'll be 30,000 by default
+# - "," logic has been changed
+
 
 import sys
+#from time import sleep
 
-def brainfuck(input):
+def brainfuck(input,stdin="",size=30*1000,ignoreloopifzero=False):
     # >	Move the pointer to the right
     # <	Move the pointer to the left
     # +	Increment the memory cell at the pointer
@@ -16,7 +29,8 @@ def brainfuck(input):
     # ]	Jump back to the matching [ if the cell at the pointer is nonzero
 
     # initialize memory
-    memory = [0] * 30*1000
+    memory = [0] * size
+    loopstack = []
     # initialize pointer
     pointer = 0
     # initialize output
@@ -30,49 +44,40 @@ def brainfuck(input):
         # print("i: {}, input[i]: {}, memory: {}, pointer: {}".format(i, input[i], memory, pointer))
         # >	Move the pointer to the right
         if input[i] == ">":
-            pointer += 1
+            pointer = ((pointer+1) % len(memory)) # Allows user to go to cell 0 if user is already at the end
         # <	Move the pointer to the left
         elif input[i] == "<":
-            pointer -= 1
+            pointer = ((pointer+len(memory)-1) % len(memory)) # Allows user to go to the last cell if user is already at the beginning
         # +	Increment the memory cell at the pointer
         elif input[i] == "+":
-            memory[pointer] += 1
+            memory[pointer] = (memory[pointer]+1) % 256
         # -	Decrement the memory cell at the pointer
         elif input[i] == "-":
-            memory[pointer] -= 1
+            memory[pointer] = (memory[pointer]+255) % 256
         # .	Output the character signified by the cell at the pointer
         elif input[i] == ".":
             output += chr(memory[pointer])
         # ,	Input a character and store it in the cell at the pointer
         elif input[i] == ",":
-            memory[pointer] = ord(input())
-        # [	Jump past the matching ] if the cell at the pointer is 0
+            if pointer < len(stdin):
+                memory[pointer] += ord(stdin[pointer]) # Fixed logic
         elif input[i] == "[":
-            if memory[pointer] == 0:
-                # find matching ]
-                count = 1
-                while count > 0:
+            if (memory[pointer]>0 or not ignoreloopifzero): 
+                loopstack.append(i)
+            else:
+                while input[i] != "]":
                     i += 1
-                    if input[i] == "[":
-                        count += 1
-                    elif input[i] == "]":
-                        count -= 1
-        # ]	Jump back to the matching [ if the cell at the pointer is nonzero
         elif input[i] == "]":
-            if memory[pointer] != 0:
-                # find matching [
-                count = 1
-                while count > 0:
-                    i -= 1
-                    if input[i] == "]":
-                        count += 1
-                    elif input[i] == "[":
-                        count -= 1
+            if memory[pointer] > 0:
+                i = loopstack[-1]-1
+            else:
+                loopstack.pop()
         else:
             pass
         i += 1
+        #sleep(0.01)
 
-    # print(memory)
+    #print(memory)
     return output
 
 # init
@@ -124,7 +129,9 @@ def brainfuck(input):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
+        #brainfuck("++++++[>+<-]",size=32)
         with open(sys.argv[1], 'r') as file:
-            print(brainfuck(file.read()))
+            print(brainfuck("".join(file.readlines()),sys.argv[2] if len(sys.argv) > 2 else "",int(sys.argv[3]) if len(sys.argv) > 3 else 30*1000,bool(sys.argv[4]) if len(sys.argv) > 4 else False)) # Implemented multi-line support and stdin support
     else:
-        print("Usage: python3 brainfuck.py [file]")
+        print("Usage: py brainfuck.py *[file] [stdin] [size]")
+        print("*: required")
